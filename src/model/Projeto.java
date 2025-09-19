b00beb445fe2db6a160ae76d4bcc0fa8d5d7d413
Larissa <larissilvas10@gmail.com>
2025-09-19 16:24:30 -0300

    atualizando

--- src/model/Projeto.java
+++ src/model/Projeto.java
@@ -1,29 +1,42 @@
 package src.model;
 
-public class Projeto {
+public class Usuario {
     private int id;
     private String nome;
-    private String descricao;
-    private String status;
+    private String cpf;
+    private String email;
+    private String login;
+    private String senhaHash;
+    private String perfil; // ADMIN, GERENTE, COLABORADOR
+    private Integer equipeId;
 
-    // Construtor
-    public Projeto(int id, String nome, String descricao, String status) {
+    public Usuario() {}
+
+    public Usuario(int id, String nome, String cpf, String email, String login, String senhaHash, String perfil, Integer equipeId) {
         this.id = id;
         this.nome = nome;
-        this.descricao = descricao;
-        this.status = status;
+        this.cpf = cpf;
+        this.email = email;
+        this.login = login;
+        this.senhaHash = senhaHash;
+        this.perfil = perfil;
+        this.equipeId = equipeId;
     }
 
-    // Getters e Setters
+    // getters e setters
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
-
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
-
-    public String getDescricao() { return descricao; }
-    public void setDescricao(String descricao) { this.descricao = descricao; }
-
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
-}
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getLogin() { return login; }
+    public void setLogin(String login) { this.login = login; }
+    public String getSenhaHash() { return senhaHash; }
+    public void setSenhaHash(String senhaHash) { this.senhaHash = senhaHash; }
+    public String getPerfil() { return perfil; }
+    public void setPerfil(String perfil) { this.perfil = perfil; }
+    public Integer getEquipeId() { return equipeId; }
+    public void setEquipeId(Integer equipeId) { this.equipeId = equipeId; }
