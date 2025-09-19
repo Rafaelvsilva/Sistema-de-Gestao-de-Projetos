d1799ecfe6755ca27a30931feb0d8b5818bdd2cd
Larissa <larissilvas10@gmail.com>
2025-09-19 16:26:33 -0300

    atualizando

--- src/model/Projeto.java
+++ src/model/Projeto.java
@@ -10,8 +10,10 @@ public class Usuario {
     private String perfil; // ADMIN, GERENTE, COLABORADOR
     private Integer equipeId;
 
+    // Construtor vazio
     public Usuario() {}
 
+    // Construtor completo
     public Usuario(int id, String nome, String cpf, String email, String login, String senhaHash, String perfil, Integer equipeId) {
         this.id = id;
         this.nome = nome;
@@ -23,20 +25,28 @@ public class Usuario {
         this.equipeId = equipeId;
     }
 
-    // getters e setters
+    // Getters e Setters
     public int getId() { return id; }
     public void setId(int id) { this.id = id; }
+
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
+
     public String getCpf() { return cpf; }
     public void setCpf(String cpf) { this.cpf = cpf; }
+
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+
     public String getLogin() { return login; }
     public void setLogin(String login) { this.login = login; }
+
     public String getSenhaHash() { return senhaHash; }
     public void setSenhaHash(String senhaHash) { this.senhaHash = senhaHash; }
+
     public String getPerfil() { return perfil; }
     public void setPerfil(String perfil) { this.perfil = perfil; }
+
     public Integer getEquipeId() { return equipeId; }
     public void setEquipeId(Integer equipeId) { this.equipeId = equipeId; }
+}
