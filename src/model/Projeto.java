2b096caf05ffb4856e75e6ec705705bb93e70087
Larissa <larissilvas10@gmail.com>
2025-09-19 16:06:50 -0300

    atualizando

---
+++ src/model/Projeto.java
@@ -0,0 +1,29 @@
+package src.model;
+
+public class Projeto {
+    private int id;
+    private String nome;
+    private String descricao;
+    private String status;
+
+    // Construtor
+    public Projeto(int id, String nome, String descricao, String status) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.status = status;
+    }
+
+    // Getters e Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getDescricao() { return descricao; }
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+}
